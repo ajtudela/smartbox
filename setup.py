d705841d32d3e8d0d2434b024f4210dd35950f01
Delmael <Delmael@users.noreply.github.com>
2025-01-20 22:06:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="smartbox",
-    version="0.0.5",
+    version="2.0.0-beta.2",
     author="Graham Bennett",
     author_email="graham@grahambennett.org",
     description="Python API to control heating 'smart boxes'",
