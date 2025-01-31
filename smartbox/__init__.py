6ad5006c1e51d0051261b403180227f7ec85f229
Edouard Andrieu de Levis <edouard.andrieu-de-levis@outlook.com>
2025-01-31 08:58:27 +0100

    add to init

--- smartbox/__init__.py
+++ smartbox/__init__.py
@@ -1,6 +1,12 @@
-from .error import APIUnavailable, InvalidAuth, SmartboxError  # noqa: F401
+from .error import (
+    APIUnavailable,  # noqa: F401
+    InvalidAuth,  # noqa: F401
+    SmartboxError,  # noqa: F401
+    ResailerNotExist,  # noqa: F401
+)
 from .session import AsyncSmartboxSession, Session  # noqa: F401
 from .socket import SocketSession  # noqa: F401
 from .update_manager import UpdateManager  # noqa: F401
+from .resailer import AvailableResailers, SmartboxResailer  # noqa: F401
 
 __version__ = "2.1.0-beta.1"
